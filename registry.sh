2515abad5fae0631705bd677c94a096d57694300
MAUREL Yoann <yoann.maurel@irisa.fr>
2019-02-11 11:37:17 +0100

    fix classpath

--- registry.sh
+++ registry.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# -J-Djava.rmi.server.codebase="file://$PWD/bin/" si UseCodebaseOnly=true
+# -J-Djava.rmi.server.codebase="file://$PWD/target/classes/" si UseCodebaseOnly=true
 
-rmiregistry -J-Djava.security.manager -J-Djava.security.policy=chat.policy -J-Dsun.rmi.log.debug=true -J-Dsun.rmi.server.exceptionTrace=true -J-Dsun.rmi.loader.logLevel=verbose -J-Dsun.rmi.dgc.logLevel=verbose -J-Dsun.rmi.transport.logLevel=verbose -J-Dsun.rmi.transport.tcp.logLevel=verbose -J-Djava.rmi.server.useCodebaseOnly=false
+rmiregistry -J-Djava.security.manager -J-Djava.security.policy=chat.policy -J-Dsun.rmi.log.debug=true -J-Dsun.rmi.server.exceptionTrace=true -J-Dsun.rmi.loader.logLevel=verbose -J-Dsun.rmi.dgc.logLevel=verbose -J-Dsun.rmi.transport.logLevel=verbose -J-Dsun.rmi.transport.tcp.logLevel=verbose -J-Djava.rmi.server.codebase="file://$PWD/target/classes/"
